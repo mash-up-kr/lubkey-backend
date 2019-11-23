aa8df9c7d43e5323f6b48b485f44ecefc8e405bb
sdh2398 <ehdgus2398@gmail.com>
2019-11-23 17:15:39 +0900

    import project

---
+++ src/main/java/com/mashup/luvket/LuvketApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.luvket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuvketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuvketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mashup/luvket/LuvketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashup.luvket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuvketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
