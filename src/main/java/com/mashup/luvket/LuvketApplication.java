051b58f878f271cd6faf0c3fa6caa34f1993265e
vo0a <vo0a195@gmail.com>
2019-12-25 01:16:41 +0900

    [Chore] 주석 변경, jackson 전략 설정

--- src/main/java/com/mashup/luvket/LuvketApplication.java
+++ src/main/java/com/mashup/luvket/LuvketApplication.java
@@ -2,9 +2,22 @@ package com.mashup.luvket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class LuvketApplication {
+/*
+
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(LuvketApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+	}
+*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(LuvketApplication.class, args);
