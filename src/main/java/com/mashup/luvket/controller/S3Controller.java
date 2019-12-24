5569cf74b797125118e8b2e703ff63408ec6ecce
vo0a <vo0a195@gmail.com>
2019-12-24 23:44:53 +0900

    [Annotate] 'S3에 사진 업로드 후 링크반환하기' 주석처리.

---
+++ src/main/java/com/mashup/luvket/controller/S3Controller.java
@@ -0,0 +1,29 @@
+/*
+package com.mashup.luvket.controller;
+
+import com.mashup.luvket.s3.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Controller
+public class S3Controller {
+
+    private final S3Uploader s3Uploader;
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(@RequestParam("data") MultipartFile multipartFile) throws IOException {
+        return s3Uploader.upload(multipartFile, "static");
+    }
+}
+*/
